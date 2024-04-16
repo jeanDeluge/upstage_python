6e076c58fd5429a8d49596792787a27814361435
NoopV <tommycho95@gmail.com>
2024-04-16 09:29:33 +0900

    stt.py

--- stt.py
+++ stt.py
@@ -16,24 +16,30 @@ import subprocess
 #         )
 #         f.write(out.stdout)
 
-
+# whisper_result(): 음성을 받고 특정 키워드를 찾는 함수를 만듦.
 def whisper_result():
     print("들어왔음")
+    # subprocess-> whisper-> wav파일을 한국어 텍스트로 변환 -> 결과를 Out에 저장
     out = subprocess.run(
         ["whisper", "output.wav", "--language", "Korean"], capture_output=True,
     )
     print("실행은 되었음")
+    # out(변환된 텍스트)를 윈도우면 CP949,아니면 utf-8로 디코딩 -> result변수에 저장
     if platform.system()=="Windows":
         result = out.stdout.decode(encoding="CP949")
     else : 
         result= out.stdout.decode(encoding="utf-8")
+    print(result)
     # model = whisper.load_model("base")
     # result = model.transcribe('C:/Users/seohyegyo/Desktop/upstage_python/output.wav')
-    print(result)
+
+    #result(디코딩한 텍스트)를 find_keyword함수를 통해 특정 키워드를 찾아 return 
     return find_keyword(result)  # {"result": "내가 말한거"}
 
-
+# 문자열을 입력 받아 딕셔너리를 반환하는 find_keyword 함수 정의
+# sentence라는 매개변수를 통해 find_keyword의 값이 전달
 def find_keyword(sentence: str) -> dict:
+    # 검색할 도시 list
     citys = [
         "뉴욕",
         "런던",
@@ -46,9 +52,10 @@ def find_keyword(sentence: str) -> dict:
         "싱가포르",
         "워싱턴DC",
     ]
-
+    # bot에 명령할 명령어 list 처리 
+    # 도시 외 명령어 
     command_list = ["날씨", "뉴스", "현지시각", "시각", "시간","요약","요약해","요약해줘","요약해죠","류스"]
-
+    # 도시 명령어 (bot이 잘못 출력한 도시 이름들?)
     city_list = [
         "뉴욕",
         "유용",
@@ -77,15 +84,22 @@ def find_keyword(sentence: str) -> dict:
     # 예외처리: 무음, 문장에 키워드가 없을 때.
     city_result = []
     command_result = []
+    # sentence(매개변수)에 전달된 str을 공백을 기준으로 분리하여 received_sentence변수에 리스트 형태로 저장
     received_sentence = sentence.split()
+
+    # for루프 내부에서 루프 변수는 선언되고 초기화되며 각 반복에서 새로운 값을 가지게 된다.  
+    # 이러한 루프 변수를 iterator(반복자)변수라고 한다.(keyword,cityname,command)
+
+# 도시 및 명령어 리스트에 추가할 단어 설정      
+    # city_result: 도시명 추가 리스트  
     for keyword in city_list:
         if keyword in received_sentence:
             city_result.append(keyword)
-
+    # command_result:명령어문 추가 리스트 
     for keyword in command_list:
         if keyword in received_sentence:
             command_result.append(keyword)
-
+# 도시 이름 정제처리 
     for cityname in city_result:
 
         if cityname == "유용":
@@ -114,7 +128,7 @@ def find_keyword(sentence: str) -> dict:
         ]:
             city_result.remove(cityname)
             city_result.append("워싱턴DC")
-
+# 명령어 정제처리 
     for command in command_result:
         if command in ["시간", "시각"]:
             command_result.remove(command)
